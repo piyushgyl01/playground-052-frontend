bbdde3f1733361612d6b92854e339770247b77fb
piyush goyal <piyush2022ug@gmail.com>
2025-05-14 12:23:11 +0530

    feat: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Post from "./pages/Post";
+import Update from "./pages/Update";
+// import { AuthProvider } from "./contexts/authContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    // <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/post" element={<Post />} />
+            <Route path="/update/:id" element={<Update />} />
+          </Route>
+        </Routes>
+      </Router>
+    // </AuthProvider>
+  );
 }
